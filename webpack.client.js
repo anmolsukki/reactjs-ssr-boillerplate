7ecadc2cbf7622ee5f8d77364f0e1de8524fdd25
anmol <anmol.s@gyftr.com>
2020-12-30 18:06:31 +0530

    Main.js File name Fixed

--- webpack.client.js
+++ webpack.client.js
@@ -3,7 +3,7 @@ const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base.js');
 
 const config = {
-  entry: './src/Client/index.js',
+  entry: './src/Client/Main.js',
   output: {
     filename: 'dist.js',
     path: path.resolve(__dirname, 'public'),
